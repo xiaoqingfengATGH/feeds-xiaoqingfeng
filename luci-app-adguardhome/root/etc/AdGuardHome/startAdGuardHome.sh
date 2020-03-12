b713bb78e173864c7c6929bc6277b470fd562d96
xiaoqingfengATGH <xiaoqingfengatgm@gmail.com>
2020-03-11 23:30:42 -0700

    AdGuardHome will automatically change its ip with router br-lan ip address.

--- luci-app-adguardhome/root/etc/AdGuardHome/startAdGuardHome.sh
+++ luci-app-adguardhome/root/etc/AdGuardHome/startAdGuardHome.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 
+ROUTER_IP=$(ifconfig br-lan | grep 'inet addr' | sed 's/^.*addr://g' | cut -d ' ' -f 1)
+IP_INCONFIG=$ROUTER_IP
+
+if [ -f "/etc/AdGuardHome/AdGuardHome.yaml" ];
+then
+IP_IN_CONFIG=cat /etc/AdGuardHome/AdGuardHome.yaml | grep "bind_host:" | sed -n 1p | cut -d ':' -f 2 | sed -r 's/^[ ]+//g'
+fi
+
 if [ ! -f "/etc/AdGuardHome/AdGuardHome.yaml" ];
 then
-	/etc/AdGuardHome/updateAdGuardHomeIP.sh
+        /etc/AdGuardHome/initConfig.sh
 fi
+
+if [ "$IP_IN_CONFIG" != "$ROUTER_IP" ];
+then
+echo "Router lan ip changed. Automatic change ip address in /etc/AdGuardHome/AdGuardHome.yaml."
+sed -i "s/bind_host: "$IP_IN_CONFIG"/bind_host: "$ROUTER_IP"/" /etc/AdGuardHome/AdGuardHome.yaml
+echo "Bind address in /etc/AdGuardHome/AdGuardHome.yaml has been changed to "$ROUTER_IP"."
+fi
+
 /usr/bin/AdGuardHome/AdGuardHome -c /etc/AdGuardHome/AdGuardHome.yaml

5c8ea905501870d92f2a056b3f3576793f88ec0e
xiaoqingfengATGH <xiaoqingfengatgm@gmail.com>
2020-03-14 02:19:34 -0700

    Update automatic br-lan trace mechanism

--- luci-app-adguardhome/root/etc/AdGuardHome/startAdGuardHome.sh
+++ luci-app-adguardhome/root/etc/AdGuardHome/startAdGuardHome.sh
@@ -17,7 +17,7 @@ if [ "$IP_IN_CONFIG" != "$ROUTER_IP" ];
 then
 echo "Router lan ip changed. Automatic change ip address in /etc/AdGuardHome/AdGuardHome.yaml."
 echo "Change bind_host form "$IP_IN_CONFIG" to "$ROUTER_IP"."
-sed -i "s/bind_host: "$IP_IN_CONFIG"/bind_host: "$ROUTER_IP"/" /etc/AdGuardHome/AdGuardHome.yaml
+sed -i "s/bind_host: .*$/bind_host: "$ROUTER_IP"/g" /etc/AdGuardHome/AdGuardHome.yaml
 echo "Bind address in /etc/AdGuardHome/AdGuardHome.yaml has been changed to "$ROUTER_IP"."
 fi
 

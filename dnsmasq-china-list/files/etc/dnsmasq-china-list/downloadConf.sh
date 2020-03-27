d3f312f2750a07e41be48d9b600aead4a827dbca
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-03-27 12:58:54 +0800

    Add wget dependency. Improve downloadConf.sh， files from server will always overwrite local scripts.

--- dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
@@ -8,10 +8,10 @@ CONF_DIR=/etc/dnsmasq-china-list/template
 
 echo "Downloading latest configurations..."
 for _conf in "${CONF_WITH_SERVERS[@]}"; do
-        wget -P $CONF_DIR https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf
+        wget -P $CONF_DIR https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $_conf.conf
 done
 for _conf in "${CONF_SIMPLE[@]}"; do
-        wget -P $CONF_DIR https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf
+        wget -P $CONF_DIR https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $_conf.conf
 done
 
 echo "Download finished. All conf saved to "$CONF_DIR"."

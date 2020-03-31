81b3c3740ba0dfa41173297f0aef805bd33f9c5b
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-03-31 19:56:32 +0800

    fix dnsmasq dynamic scripts location

--- dnsmasq-china-list/files/etc/dnsmasq-china-list/clearConf.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/clearConf.sh
@@ -2,12 +2,13 @@
 
 LIST_FILE_PREFIX="chnlist_"
 INSTALL_LOC=/etc/dnsmasq-china-list
+TARGET_LOC=/tmp/dnsmasq.d
 source $INSTALL_LOC/conflist.sh
 
 echo "Removing old configurations..."
 #for _conf in "${CONF_WITH_SERVERS[@]}" "${CONF_SIMPLE[@]}"; do
-#  rm -f /etc/dnsmasq.d/"$_conf"*.conf
+#  rm -f $TARGET_LOC/"$_conf"*.conf
 #done
 
-rm -f /etc/dnsmasq.d/"$LIST_FILE_PREFIX"*.conf
+rm -f $TARGET_LOC/"$LIST_FILE_PREFIX"*.conf
 echo "All dnsmasq_china_list conf removed."
--- dnsmasq-china-list/files/etc/dnsmasq-china-list/refreshConf.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/refreshConf.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 INSTALL_LOC=/etc/dnsmasq-china-list
+TARGET_LOC=/tmp/dnsmasq.d
 
 LIST_FILE_PREFIX="chnlist_"
 CONF_DIR=/etc/dnsmasq-china-list/template
 source $INSTALL_LOC/conflist.sh
 source $INSTALL_LOC/dnslist.sh
 
+[ ! -d $TARGET_LOC ] && mkdir -p $TARGET_LOC
+
 $INSTALL_LOC/clearConf.sh
 
 echo "Installing new configurations..."
 for _conf in "${CONF_SIMPLE[@]}"; do
-  cp "$CONF_DIR/$_conf.conf" "/etc/dnsmasq.d/$LIST_FILE_PREFIX$_conf.conf"
+  cp "$CONF_DIR/$_conf.conf" "$TARGET_LOC/$LIST_FILE_PREFIX$_conf.conf"
 done
 
 for _server in "${SERVERS[@]}"; do
   for _conf in "${CONF_WITH_SERVERS[@]}"; do
-    cp "$CONF_DIR/$_conf.conf" "/etc/dnsmasq.d/$LIST_FILE_PREFIX$_conf.$_server.conf"
+    cp "$CONF_DIR/$_conf.conf" "$TARGET_LOC/$LIST_FILE_PREFIX$_conf.$_server.conf"
   done
 
-  sed -i "s|^\(server.*\)/[^/]*$|\1/$_server|" /etc/dnsmasq.d/*."$_server".conf
+  sed -i "s|^\(server.*\)/[^/]*$|\1/$_server|" $TARGET_LOC/*."$_server".conf
 done

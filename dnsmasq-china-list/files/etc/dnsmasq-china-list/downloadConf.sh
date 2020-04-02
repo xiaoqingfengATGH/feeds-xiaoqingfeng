887357be052ef5ab1899e705328d6208d201a7f7
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-04-02 23:01:19 +0800

    fix download conf path

--- dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
@@ -4,14 +4,14 @@ set -e
 INSTALL_LOC=/etc/dnsmasq-china-list
 source $INSTALL_LOC/conflist.sh
 
-CONF_DIR=/etc/dnsmasq-china-list/template
+CONF_DIR=/etc/dnsmasq-china-list/template/
 
 echo "Downloading latest configurations..."
 for _conf in "${CONF_WITH_SERVERS[@]}"; do
-        wget -P $CONF_DIR https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $_conf.conf
+        wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
 done
 for _conf in "${CONF_SIMPLE[@]}"; do
-        wget -P $CONF_DIR https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $_conf.conf
+        wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
 done
 
 echo "Download finished. All conf saved to "$CONF_DIR"."

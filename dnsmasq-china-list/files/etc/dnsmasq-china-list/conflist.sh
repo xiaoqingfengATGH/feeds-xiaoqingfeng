9890529fd41452c29f3bfd46507d62a046eb4d7a
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-05-01 11:52:50 +0800

    enable google.china acceleration
    and update all acceleration configs

--- dnsmasq-china-list/files/etc/dnsmasq-china-list/conflist.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/conflist.sh
@@ -1,6 +1,6 @@
 CONF_WITH_SERVERS=()
 CONF_WITH_SERVERS[0]=accelerated-domains.china
 CONF_WITH_SERVERS[1]=apple.china 
-#CONF_WITH_SERVERS[2]=google.china
+CONF_WITH_SERVERS[2]=google.china
 CONF_SIMPLE=()
 CONF_SIMPLE[0]=bogus-nxdomain.china

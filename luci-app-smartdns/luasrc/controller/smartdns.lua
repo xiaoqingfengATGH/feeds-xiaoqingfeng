846f91bcdfa027470d5d9c52aa705a582d55426b
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-05-07 23:28:43 +0800

    Adjust luci-app-smartdns menu entry

--- luci-app-smartdns/luasrc/controller/smartdns.lua
+++ luci-app-smartdns/luasrc/controller/smartdns.lua
@@ -23,11 +23,11 @@ function index()
 	end
 
 	local page
-	page = entry({"admin", "services", "smartdns"}, cbi("smartdns/smartdns"), _("SmartDNS"), 60)
+	page = entry({"admin", "dns", "smartdns"}, cbi("smartdns/smartdns"), _("SmartDNS"), 60)
 	page.dependent = true
-	page = entry({"admin", "services", "smartdns", "status"}, call("act_status"))
+	page = entry({"admin", "dns", "smartdns", "status"}, call("act_status"))
 	page.leaf = true
-	page = entry({"admin", "services", "smartdns", "upstream"}, cbi("smartdns/upstream"), nil)
+	page = entry({"admin", "dns", "smartdns", "upstream"}, cbi("smartdns/upstream"), nil)
 	page.leaf = true
 end
 

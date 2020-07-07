31741dd6dc3903b933743ae2c38bc908c422804b
Richard Yu <31612198+xiaoqingfengATGH@users.noreply.github.com>
2020-07-07 22:40:47 +0800

    Use raw.fastgit.org as config download url

--- dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
@@ -8,10 +8,10 @@ CONF_DIR=/etc/dnsmasq-china-list/template/
 
 echo "Downloading latest configurations..."
 for _conf in "${CONF_WITH_SERVERS[@]}"; do
-        wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
+        wget https://raw.fastgit.org/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
 done
 for _conf in "${CONF_SIMPLE[@]}"; do
-        wget https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
+        wget https://raw.fastgit.org/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
 done
 
 echo "Download finished. All conf saved to "$CONF_DIR"."

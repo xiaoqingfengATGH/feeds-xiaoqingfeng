74b53a6d014a7ee590406c1d3898a90209d25b00
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2020-07-08 20:54:09 +0800

    dnsmasq-china-list support download configuration integrity check

--- dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/downloadConf.sh
@@ -7,11 +7,33 @@ source $INSTALL_LOC/conflist.sh
 CONF_DIR=/etc/dnsmasq-china-list/template/
 
 echo "Downloading latest configurations..."
-for _conf in "${CONF_WITH_SERVERS[@]}"; do
-        wget https://raw.fastgit.org/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
+
+#for _conf in "${CONF_WITH_SERVERS[@]}"; do
+#        wget https://raw.fastgit.org/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
+#done
+#for _conf in "${CONF_SIMPLE[@]}"; do
+#        wget https://raw.fastgit.org/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
+#done
+
+TMP_FOLDER=/tmp/dnsmasq-china-list
+DOWNLOAD_VER=$(cat $TMP_FOLDER/version | awk -F' ' '{print $2}')
+
+DOWNLOAD_SUCC=0
+for i in 1 2 3; do
+	wget https://gitee.com/xiaoqingfeng999/dnsmasq-china-list-pkgs/raw/master/configs.7z -O $TMP_FOLDER/configs.7z
+	DOWNLOAD_PKG_MD5=$(md5sum $TMP_FOLDER/configs.7z | awk -F' ' '{print $1}')
+	if [ "$DOWNLOAD_VER" = "$DOWNLOAD_PKG_MD5" ]; then
+		DOWNLOAD_SUCC=1
+		break
+	fi
 done
-for _conf in "${CONF_SIMPLE[@]}"; do
-        wget https://raw.fastgit.org/felixonmars/dnsmasq-china-list/master/$_conf.conf  -O $CONF_DIR$_conf.conf
-done
+
+if [ "$DOWNLOAD_SUCC" = "0" ]; then
+	echo "Download update failed!"
+	exit 1
+fi
+
+rm -f $CONF_DIR""*.conf
+7z e -o$CONF_DIR $TMP_FOLDER/configs.7z
 
 echo "Download finished. All conf saved to "$CONF_DIR"."
--- dnsmasq-china-list/files/etc/dnsmasq-china-list/update.sh
+++ dnsmasq-china-list/files/etc/dnsmasq-china-list/update.sh
@@ -1,6 +1,32 @@
 #!/bin/sh
 INSTALL_LOC=/etc/dnsmasq-china-list
+
+CONF_DIR=$INSTALL_LOC/template/
+
+TMP_FOLDER=/tmp/dnsmasq-china-list
+[ -d $TMP_FOLDER ] && rm -rf $TMP_FOLDER
+mkdir -p $TMP_FOLDER
+
+CURRENT_VERSION_FILE=$CONF_DIR""version
+
+wget https://gitee.com/xiaoqingfeng999/dnsmasq-china-list-pkgs/raw/master/version -O $TMP_FOLDER/version
+
+DOWNLOAD_VER=$(cat $TMP_FOLDER/version | awk -F' ' '{print $2}')
+CURRENT_VER="NA"
+[ -r $CURRENT_VERSION_FILE ] && CURRENT_VER=$(cat $CURRENT_VERSION_FILE | awk -F' ' '{print $2}')
+
+if [ "$CURRENT_VER" = "$DOWNLOAD_VER" ]; then
+	echo "The current version is already the latest version."
+	exit 0
+fi
+
+echo "New updates available."
+
 $INSTALL_LOC/downloadConf.sh
+
+cp $TMP_FOLDER/version $CURRENT_VERSION_FILE
+echo Version mark has been updated.
+
 $INSTALL_LOC/refreshConf.sh
 $INSTALL_LOC/flush.sh
 echo dnsmasq_china_list updated.

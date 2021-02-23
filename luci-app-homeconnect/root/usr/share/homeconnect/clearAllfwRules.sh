082caadaf2f90b4c40b90922679bcc02bc7fa7a0
xiaoqingfeng <xiaoqingfengatgm@gmail.com>
2021-02-23 23:17:56 +0800

    luci-app-homeconnect : bump to 1.1
    improve firewall's rule generation

---
+++ luci-app-homeconnect/root/usr/share/homeconnect/clearAllfwRules.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+fwRC=$(cat /etc/config/firewall | grep rule | wc -l)
+fwMI=$(expr $fwRC - 1)
+change=0
+for ((i=($fwRC);i>=0;i--));
+do
+	ruleName=$(uci -q get firewall.@rule[$i].name)
+	if [ -n ${ruleName} ] && [ "${ruleName:0:3}" == "HC_" ]; then
+		uci delete firewall.@rule[$i]
+		change=1
+	fi
+done
+
+[ "$change" == "1" ] && {
+	uci commit firewall >/dev/null 2>&1
+	[  "withFWRestart" == "$1" ] && /etc/init.d/firewall restart >/dev/null 2>&1
+}
